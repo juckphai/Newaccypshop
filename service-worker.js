03cfd07618903901e70b9f0e40331a913f39711a
juckphai <llsvuslnw@gmail.com>
2026-05-18 15:26:39 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
 // ปรับปรุงให้รองรับ Firebase + Offline เต็มรูปแบบ และแก้ปัญหาการเชื่อมต่อ Database
-const staticCacheName = 'account-app-static-v72019'; // อัพเดทเวอร์ชัน Cache เป็น v709
-const dynamicCacheName = 'account-app-dynamic-v72019';
+const staticCacheName = 'account-app-static-v19'; // อัพเดทเวอร์ชัน Cache เป็น v709
+const dynamicCacheName = 'account-app-dynamic-v19';
 
 // ไฟล์ที่ต้อง cache ตั้งแต่ตอน install
 const assets = [
